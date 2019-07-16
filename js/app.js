4e3a9887ee4cb5895addd90913f34f533b711de0
Chris Beaney <chrisbeaney@gmail.com>
2019-07-16 12:16:12 +0100

    Checked for completed lines

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const tetrominoArray = []
 let currentBlock = null
 let currentPosition = 4
 let timerId = null
-
+let score = 0
 
 document.addEventListener('DOMContentLoaded',() => {
   // 0. Create DOM variables
@@ -26,13 +26,13 @@ document.addEventListener('DOMContentLoaded',() => {
 
   // 2. Create Blocks
   function createTetrominos () {
-    const shapeO = [0, 1, width, 1 + width] // [0,1,10,11]
+    const shapeO = [0, 1, width, 1 + width]
     const shapeI = [0, 1, 2, 3]
-    const shapeT = [0, 1, 2, 1 + width] // [0,1,2,11]
-    const shapeL = [0, 1, 2, width] // [0,1,2,10]
-    const shapeJ = [0, 1, 2, 2 + width] // [0,1,2,12]
-    const shapeS = [1, 2, width, 1 + width] // [1,2,10,11]
-    const shapeZ = [0, 1, 1 + width, 2 + width] // [0,1,11,12]
+    const shapeT = [0, 1, 2, 1 + width]
+    const shapeL = [0, 1, 2, width]
+    const shapeJ = [0, 1, 2, 2 + width]
+    const shapeS = [1, 2, width, 1 + width]
+    const shapeZ = [0, 1, 1 + width, 2 + width]
 
     tetrominoArray.push(shapeO)
     tetrominoArray.push(shapeI)
@@ -43,6 +43,25 @@ document.addEventListener('DOMContentLoaded',() => {
     tetrominoArray.push(shapeZ)
   }
 
+  // 2a. Add Tetromino rotations, (none required for '0' block).
+  const shapeI90 = [0, width, 2*width, 3*width]
+  const shapeI180 = [3,2,1,0]
+  const shapeI270 = [3*width, 2*width, width, 0]
+  const shapeT90 = []
+  const shapeT180 = []
+  const shapeT270 = []
+  const shapeL90 = []
+  const shapeL180 = []
+  const shapeL270 = []
+  const shapeJ90 = []
+  const shapeJ180 = []
+  const shapeJ270 = []
+  const shapeS90 = []
+  const shapeS180 = []
+  const shapeS270 = []
+  const shapeZ90 = []
+  const shapeZ180 = []
+  const shapeZ270 = []
 
   // 3. Pick a random tetromino.
   function createBlock () {
@@ -69,6 +88,8 @@ document.addEventListener('DOMContentLoaded',() => {
     if(currentBlock.some(cannotMove)) {
       lockBlocks()
       clearInterval(timerId)
+      // Make sure we check for completed lines before gameOver().
+      checkRows()
       gameOver()
       return initialise()
     }
@@ -95,15 +116,17 @@ document.addEventListener('DOMContentLoaded',() => {
       case 37:
       // TODO: Will likely be necessary to update once rotating implemented.
       // TODO: add horizontal collision prevention
-        if(currentPosition % width !== 0) currentPosition -= 1
+        // if(currentBlock.some(index => (currentPosition - index) % width === 0)) {
+        //   break
+        // } else
+        if(currentPosition % width !== 0 && !collisionLeft()) currentPosition -= 1
         break
       // Moving right
       case 39:
         // TODO: add horizontal collision prevention
-        if(currentBlock.some(index => (currentPosition + index) % width === width - 1)) {
+        if((currentBlock.some(index => (currentPosition + index) % width === width - 1)) || collisionRight()) {
           break
         } else if (currentPosition % width < width - 1) currentPosition += 1
-        console.log(currentPosition)
         break
       // Moving down
       case 40:
@@ -115,17 +138,69 @@ document.addEventListener('DOMContentLoaded',() => {
 
   }
 
-  // 8. Detect Sides - do this in blockMove()?
+  // 8. Detect Horizontal collisions with 'locked' squares - run function in blockMove()
+  function collisionLeft () {
+    let collision = false
+    // If square to the left or to the right is 'locked'
+    // set collision to true and return from the function.
+    if(currentBlock.forEach(index => squares[index + currentPosition - 1].classList.contains('locked'))) {
+      collision = true
+      return collision
+    }
+  }
 
-  // 9. Check for successful rows
+  function collisionRight () {
+    let collision = false
+    // If square to the left or to the right is 'locked'
+    // set collision to true and return from the function.
+    if(currentBlock.forEach(index => squares[index + currentPosition + 1].classList.contains('locked'))) {
+      collision = true
+      return collision
+    }
+  }
+
+  // 9a. Check for successful rows
   function checkRows () {
-    // Look through the squares 10 at a time.
-    // squares.forEach(index => )
-    // Can I use currentPosition to avoid checking through the whole squares array.
-    // If all contain the class 'locked':
-    // Remove the 'locked' class to clear the line.
-    // Drop any locked blocks above.
-    // Increment score.
+    // Take chunks of squares 10 at a time.
+    for (let i=0; i<squares.length; i+=10) {
+      let counter = 0
+      // Take each of the 10 squares passed.
+      for (let j=0; j<10; j++) {
+        if(squares[i+j].classList.contains('locked')) {
+          counter++
+        } else {
+          break
+        }
+        if(counter === 10) {
+          // TODO: Make blocks above fall.
+          // TODO: Increment score.
+          clearLines(i,j)
+          score ++
+        }
+      }
+    }
+  }
+
+  // 9b. clearLines after a successful row.
+  function clearLines(startIndex, stopIndex) {
+    for(let i=startIndex; i<=startIndex + stopIndex; i++) {
+      squares[i].classList.remove('block')
+      squares[i].classList.remove('locked')
+      lineDown(startIndex, stopIndex)
+    }
+  }
+
+  // 9c. Drag lines above down.
+  function lineDown (startIndex, stopIndex) {
+    console.log(`lineDown() running with start of ${startIndex} and end of ${stopIndex}`)
+    for(let i=startIndex; i<=stopIndex; i++) {
+      if(squares[i-width].classList.contains('locked')) {
+        squares[i-width].classList.remove('block')
+        squares[i-width].classList.remove('locked')
+        squares[i].classList.add('block')
+        squares[i].classList.add('locked')
+      }
+    }
   }
 
   // 10. Block rotation function
